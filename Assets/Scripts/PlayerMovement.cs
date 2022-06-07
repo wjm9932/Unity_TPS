e265eb4a30b75ac3a5f02982e2829666c51abef8
wjm9932 <wjm9932@gmail.com>
2022-06-07 21:30:27 +0900

    TPS

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private PlayerInput playerInput;
+    private Animator animator;
+
+    private Camera followCam;
+
+    public float speed = 6f;
+    public float jumpVelocity = 20f;
+    [Range(0.01f, 1f)] public float airControlPercent;
+
+    public float speedSmoothTime = 0.1f;
+    public float turnSmoothTime = 0.1f;
+
+    private float speedSmoothVelocity;
+    private float turnSmoothVelocity;
+
+    private float currentVelocityY;
+
+    public float currentSpeed =>
+        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+        followCam = Camera.main;
+        currentVelocityY = 0f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentSpeed > 0.2f || playerInput.fire) Rotate();
+
+        Move(playerInput.moveInput);
+
+        if (playerInput.jump) Jump();
+    }
+
+    private void Update()
+    {
+        UpdateAnimation(playerInput.moveInput);
+    }
+
+    public void Move(Vector2 moveInput)
+    {
+        var targetSpeed = moveInput.magnitude * speed;
+        var moveDir = Vector3.Normalize(transform.forward * moveInput.y + transform.right * moveInput.x);
+        float smoothTime;
+
+        if (characterController.isGrounded == true)
+        {
+            smoothTime = speedSmoothTime;
+        }
+        else
+        {
+            smoothTime = speedSmoothTime / airControlPercent;
+        }
+
+        targetSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, smoothTime);
+
+        currentVelocityY += Time.deltaTime * Physics.gravity.y;
+
+        var velocity = moveDir * targetSpeed + Vector3.up * currentVelocityY;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (characterController.isGrounded == true)
+        {
+            currentVelocityY = 0f;
+        }
+    }
+
+    public void Rotate()
+    {
+        var targetRotation = followCam.transform.eulerAngles.y;
+
+        targetRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+
+        transform.eulerAngles = Vector3.up * targetRotation;
+    }
+
+    public void Jump()
+    {
+        if (characterController.isGrounded == true)
+        {
+            currentVelocityY = jumpVelocity;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void UpdateAnimation(Vector2 moveInput)
+    {
+        var animationSpeedPercent = currentSpeed / speed;
+        animator.SetFloat("Vertical Move", moveInput.y * animationSpeedPercent, 0.05f, Time.deltaTime);
+        animator.SetFloat("Horizontal Move", moveInput.x * animationSpeedPercent, 0.05f, Time.deltaTime);
+    }
+}
